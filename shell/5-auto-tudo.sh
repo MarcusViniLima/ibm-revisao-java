f187eb70bb45b663d4f475bd459203c34b083c33
Marcus vinicius Lameu lima <91769337+MarcusViniLima@users.noreply.github.com>
2022-09-10 23:40:43 -0300

    primeira atividade

---
+++ shell/5-auto-tudo.sh
@@ -0,0 +1,13 @@
+echo "rm -rf teste/arquivos" >> run.sh
+
+echo "cd teste" >> run.sh
+
+echo "mkdir arquivos" >> run.sh
+
+echo "cd arquivos" >> run.sh
+
+echo "touch teste.txt" >> run.sh
+
+chmod +x run.sh
+
+./run.sh
